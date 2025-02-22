8c67959b7d80b9d5cac4ad0284b501be736badaf
ResoM <dongukmoon42@gmail.com>
2025-02-22 21:41:58 +0900

    feat : 자바 시작

---
+++ ch01/src/HelloWorld/HelloWorld.java
@@ -0,0 +1,12 @@
+package HelloWorld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HelloWorld!");
+		System.out.println("my name is ResoM");
+
+	}
+
+}
